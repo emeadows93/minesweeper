7b38c2c64a2c8c902ee2218c7b24dcc9ef14eccb
emeadows93 <42155402+emeadows93@users.noreply.github.com>
2018-08-29 16:26:08 -0500

    Add files via upload

--- minesweeper.js
+++ minesweeper.js
@@ -1,16 +1,49 @@
-const printBoard = board => {console.log("Current Board:")
-console.log(board[0].join(' | '));
-console.log(board[1].join(' | '));
-console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      row.push(" ");
+    }
+    board.push(row);
+  }
+  return board;
 };
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' ',],
-  [' ', ' ', ' ']
-];
 
-printBoard(board);
-board[0][1] = '1'
-board[2][2] = 'B'
 
-printBoard(board);
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+let numberOfBombsPlaced = 0;
+
+while (numberOfBombsPlaced < numberOfBombs){
+  let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+  let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+  board[randomRowIndex][randomColumnIndex] = "B";
+  numberOfBombsPlaced++;
+  /*
+  The code in your while loop has the potential
+  to place bombs on top of already existing bombs.
+  This will be fixed when you learn about control flow
+  */
+}
+return board;
+};
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(" | ")).join('\n'));
+};
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
